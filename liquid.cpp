892e0b3fbbc87b69cbc20512ac2111a66dc8a81d
enix403 <fog.code000@gmail.com>
2022-12-13 20:34:07 +0500

    Assign + Enchanced Edit Item

--- liquid.cpp
+++ liquid.cpp
@@ -27,6 +27,8 @@ enum ItemCategory
 
 static constexpr size_t MAX_NAME_LEN = 50;
 
+typedef char name_str_t[MAX_NAME_LEN];
+
 struct Member
 {
     char name[MAX_NAME_LEN];
@@ -37,6 +39,7 @@ struct Member
 };
 
 typedef uint16_t item_id_t;
+typedef uint32_t item_count_t;
 
 struct ItemMeta
 {
@@ -48,7 +51,7 @@ struct InventoryItem
 {
     item_id_t item_id = 0;
     ItemMeta meta {};
-    uint32_t item_count = 0;
+    item_count_t item_count = 0;
     Member* allocated_to = nullptr;
     bool active = true;
 };
@@ -69,11 +72,11 @@ inline constexpr uint32_t grow(uint32_t old)
     return old < 8 ? 8 : 2 * old;
 }
 
-inline Member* CreateMember(const std::string& name)
+inline Member* CreateMember(const char* name)
 {
     Member* m = new Member;
 
-    strcpy(m->name, name.c_str());
+    strcpy(m->name, name);
     m->borrow_count = 0;
     m->next = nullptr;
     m->prev = nullptr;
@@ -99,8 +102,8 @@ void OnBeforeQuit(Inventory& inv);
 
 namespace Input
 {
-    int64_t integer();
-    void entity_name();
+    int64_t integer(bool allow_empty = false);
+    bool entity_name(char* target, size_t max_len, bool allow_empty = false);
     InventoryItem* identitfied_item(const Inventory& inv, bool show_error = true);
 }; // namespace Input
 
@@ -123,6 +126,7 @@ namespace InvUserActions
     K__Result SearchItem(Inventory& inv);
     K__Result EditItem(Inventory& inv);
     K__Result DeleteItem(Inventory& inv);
+    K__Result AssignItem(Inventory& inv);
     // InvResult AssignItem(Inventory& inv, item_id_t id, const std::string& to);
     // InvResult RetrieveItem(Inventory& inv, item_id_t id, const std::string& from);
     K__Result ItemDetails(Inventory& inv);
@@ -147,7 +151,10 @@ namespace Frontend
 
     inline void DisplayItem(InventoryItem& item)
     {
-        cout << item.item_id << " | " << item.meta.name << endl;   
+        cout 
+            << item.item_id << " | "
+            << item.meta.name << " | "
+            << item.item_count << endl;
     }
 
     Action RequestAction();
@@ -166,12 +173,12 @@ int main()
 
 #if 1
     // clang-format off
-    inv.items[inv.count++] = { 1, { "Item 1", IC_MACHINERY } };
-    inv.items[inv.count++] = { 2, { "Item 2", IC_MACHINERY } };
-    inv.items[inv.count++] = { 3, { "Item 3", IC_MACHINERY } };
-    inv.items[inv.count++] = { 4, { "Item 4", IC_MACHINERY } };
-    inv.items[inv.count++] = { 5, { "Item 5", IC_MACHINERY } };
-    inv.items[inv.count++] = { 6, { "Item 6", IC_MACHINERY } };
+    inv.items[inv.count++] = { .item_id = 1, .meta={ "Item 1", IC_MACHINERY }, .item_count = 41 };
+    inv.items[inv.count++] = { .item_id = 2, .meta={ "Item 2", IC_MACHINERY }, .item_count = 41 };
+    inv.items[inv.count++] = { .item_id = 3, .meta={ "Item 3", IC_MACHINERY }, .item_count = 41 };
+    inv.items[inv.count++] = { .item_id = 4, .meta={ "Item 4", IC_MACHINERY }, .item_count = 41 };
+    inv.items[inv.count++] = { .item_id = 5, .meta={ "Item 5", IC_MACHINERY }, .item_count = 41 };
+    inv.items[inv.count++] = { .item_id = 6, .meta={ "Item 6", IC_MACHINERY }, .item_count = 41 };
     // clang-format on
 #endif
 
@@ -223,7 +230,7 @@ namespace Input
 {
     constexpr streamsize max_ssz = std::numeric_limits<streamsize>::max();
 
-    int64_t integer()
+    int64_t integer(bool allow_empty)
     {
         static const auto MAX_LINE_SIZE = 1024;
         static char line[MAX_LINE_SIZE];
@@ -232,6 +239,10 @@ namespace Input
         std::cin.getline(line, MAX_LINE_SIZE, '\n');
 
         ss << line;
+
+        // If string is empty then it's first char must be '\0'
+        if (allow_empty && line[0] == '\0')
+            return -2;
 
         int val;
         ss >> std::skipws >> val;
@@ -252,11 +263,12 @@ namespace Input
         return val < 0 ? -1 : val;
     }
 
-    bool entity_name(char* target, size_t max_len)
+    bool entity_name(char* target, size_t max_len, bool allow_empty)
     {
         static const auto MAX_LINE_SIZE = 1024;
         static char name[MAX_LINE_SIZE];
-        cin >> ws;
+        if (!allow_empty)
+            cin >> ws;
         std::cin.getline(name, MAX_LINE_SIZE, '\n');
 
         auto xc = std::cin.gcount() - 1;
@@ -286,12 +298,12 @@ namespace Input
             return nullptr;
         }
 
-        auto itemptr = InvUtil_FindItemById(inv, (item_id_t)id_);
+        auto itemptr = InvUtil_FindItemById(inv, (item_id_t) id_);
 
         if (show_error && itemptr == nullptr)
         {
             cout << "\n[ERROR] * Item with id " << id_ << " not found *\n"
-                 <<   "        * Failed to load item *\n";
+                 << "        * Failed to load item *\n";
         }
 
         return itemptr;
@@ -348,12 +360,27 @@ Select an option:
 
         switch (action)
         {
-            case Action::AddItem:       InvUserActions::AddItem(inv);       break;
-            case Action::ViewItems:     InvUserActions::ViewItems(inv);     break;
-            case Action::SearchItem:    InvUserActions::SearchItem(inv);    break;
-            case Action::EditItem:      InvUserActions::EditItem(inv);      break;
-            case Action::DeleteItem:    InvUserActions::DeleteItem(inv);    break;
-            case Action::ItemDetails:   InvUserActions::ItemDetails(inv);   break;
+            case Action::AddItem:
+                InvUserActions::AddItem(inv);
+                break;
+            case Action::ViewItems:
+                InvUserActions::ViewItems(inv);
+                break;
+            case Action::SearchItem:
+                InvUserActions::SearchItem(inv);
+                break;
+            case Action::EditItem:
+                InvUserActions::EditItem(inv);
+                break;
+            case Action::DeleteItem:
+                InvUserActions::DeleteItem(inv);
+                break;
+            case Action::AssignItem:
+                InvUserActions::AssignItem(inv);
+                break;
+            case Action::ItemDetails:
+                InvUserActions::ItemDetails(inv);
+                break;
 
             default:
                 break;
@@ -371,6 +398,7 @@ namespace InvUserActions
     K__Result AddItem(Inventory& inv)
     {
         item_id_t id;
+        item_count_t icount;
         ItemMeta meta;
 
         {
@@ -388,16 +416,30 @@ namespace InvUserActions
             if (InvUtil_FindItemById(inv, id) != nullptr)
             {
                 cout << "\n[ERROR] * Item with id " << id << " already exists. *\n"
-                     <<   "        * Failed to add item *\n";
+                     << "        * Failed to add item *\n";
 
                 return K__Result::Failed;
             }
-
         }
 
-        cout << IDN << "Enter Item name: ";
-        if (!Input::entity_name(meta.name, MAX_NAME_LEN))
-            return K__Result::Failed;
+        {
+            cout << IDN << "Enter Item name: ";
+            if (!Input::entity_name(meta.name, MAX_NAME_LEN))
+                return K__Result::Failed;
+        }
+
+        {
+            cout << IDN << "Enter Item's available unit count: ";
+            auto ic = Input::integer();
+
+            if (ic == -1)
+            {
+                cout << "\n[ERROR] * Invalid input *" << '\n';
+                return K__Result::Failed;
+            }
+
+            icount = ic;
+        }
 
         meta.cat = IC_MACHINERY;
 
@@ -426,10 +468,10 @@ namespace InvUserActions
         strcpy(slot.meta.name, meta.name);
         slot.meta.cat = meta.cat;
 
-        slot.item_count = 0;
+        slot.item_count = icount;
         slot.allocated_to = nullptr;
 
-        cout << "\nItem \"" << meta.name << "\" added successfully\n";
+        cout << "Item \"" << meta.name << "\" added successfully\n";
 
         return K__Result::Ok;
     }
@@ -450,7 +492,7 @@ namespace InvUserActions
 
     K__Result SearchItem(Inventory& inv)
     {
-        cout << IDN << "Enter Item Name: "; 
+        cout << IDN << "Enter Item Name: ";
         string str;
         getline(std::cin >> std::ws, str);
 
@@ -483,13 +525,28 @@ namespace InvUserActions
         if (item == nullptr)
             return K__Result::Failed;
 
-        cout << IDN << "Enter Item's new name: ";
-        if (!Input::entity_name(item->meta.name, MAX_NAME_LEN))
+        cout << IDN << "Enter Item's new name (press enter to keep original): ";
+        static name_str_t name;
+        if (!Input::entity_name(name, MAX_NAME_LEN, true))
             return K__Result::Failed;
+
+        cout << IDN << "Enter Item's available unit count (press enter to keep original): ";
+        auto ic = Input::integer(true);
+        if (ic == -1)
+        {
+            cout << "\n[ERROR] * Invalid input *" << '\n';
+            return K__Result::Failed;
+        }
 
         cout << "\n";
 
         item->meta.cat = IC_MACHINERY; // Edited
+
+        if (ic != -2)
+            item->item_count = ic;
+
+        if (name[0] != '\0') /* Name is not empty, i.e user has provided some input */
+            strcpy(item->meta.name, name);
 
         cout << "Item saved successfully\n";
 
@@ -508,15 +565,76 @@ namespace InvUserActions
 
         item->active = false;
 
-        cout << "Item \"" << item->meta.name
-            << "\" with id " << item->item_id
-            << " deleted successfully\n";
+        cout << "Item \"" << item->meta.name << "\" with id " << item->item_id << " deleted successfully\n";
+
+        return K__Result::Ok;
+    }
+
+    Member* FindMemberByName(Member* head, const char* name)
+    {
+        while (head != nullptr)
+        {
+            if (strcmp(head->name, name) == 0)
+                return head;
+
+            head = head->next;
+        }
+
+        return nullptr;
+    }
+
+    K__Result AssignItem(Inventory& inv)
+    {
+        cout << IDN << "Enter Item Id: ";
+        auto item = Input::identitfied_item(inv);
+        if (item == nullptr)
+            return K__Result::Failed;
+
+        if (item->item_count > 0)
+        {
+            cout << "\n<*> Assigning item \"" << item->meta.name << "\"\n";
+        }
+        else
+        {
+            cout << "\n[ERROR] * No units available for this item *" << '\n';
+            return K__Result::Failed;
+        }
+
+        static name_str_t name;
+        cout << IDN << "Enter assignee's name: ";
+        if (!Input::entity_name(name, MAX_NAME_LEN))
+            return K__Result::Failed;
+
+        cout << "\n";
+
+        auto entry = FindMemberByName(item->allocated_to, name);
+
+        if (entry == nullptr)
+        {
+            entry = CreateMember(name);
+
+            auto tail = item->allocated_to;
+
+            if (tail != nullptr)
+                tail->prev = entry;
+
+            item->allocated_to = entry;
+
+            entry->next = tail;
+            entry->prev = nullptr;
+        }
+
+        ++entry->borrow_count;
+        --item->item_count;
+
+        cout
+            << "Item \"" << item->meta.name
+            << "\" assigned to \"" << name << "\" successfully\n";
 
         return K__Result::Ok;
     }
 
 #if 0
-
     Member* FindMemberByName(Member* head, const std::string& name)
     {
         while (head != nullptr)
@@ -591,7 +709,6 @@ namespace InvUserActions
 
         return InvResult::Ok;
     }
-
 #endif
 
     K__Result ItemDetails(Inventory& inv)
